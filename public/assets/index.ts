cb61661321bb1039c8d4a7ae972eb8e37e97ac80
Wolz <wceliz.dev@gmail.com>
2023-04-23 18:47:04 -0400

    first commit

---
+++ public/assets/index.ts
@@ -0,0 +1,12 @@
+import logo from "./images/logo.png";
+import profileImg from "./images/profileImg.png";
+import projectOne from "./images/projectOne.png";
+import projectTwo from "./images/projectTwo.png";
+
+
+export {
+    logo,
+    profileImg,
+    projectOne,
+    projectTwo,
+};
