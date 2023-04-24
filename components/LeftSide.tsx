19689a9d569c21f92bff01c1608f71da7ce20059
Wolz <wceliz.dev@gmail.com>
2023-04-23 20:00:41 -0400

    feat: sides 20%

---
+++ components/LeftSide.tsx
@@ -0,0 +1,31 @@
+import { TbBrandGithub } from "react-icons/tb"
+import { SlSocialLinkedin } from "react-icons/sl"
+import { AiOutlineMail } from "react-icons/ai"
+
+const LeftSide = () => {
+    return (
+        <div
+            className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight"
+        >
+            <div className="flex flex-col gap-4 ">
+                <a href="https://github.com/Walter-Celiz" target="_blank">
+                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                        <TbBrandGithub />
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/in/walter-celiz/" target="_blank">
+                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                        <SlSocialLinkedin />
+                    </span>
+                </a>
+                <a href="mailto:wceliz.dev@gmail.com" target="_blank">
+                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                        <AiOutlineMail />
+                    </span>
+                </a>
+            </div>
+            <div className="w-[2px] h-32 bg-textDark"></div>
+        </div>
+    )
+}
+export default LeftSide
