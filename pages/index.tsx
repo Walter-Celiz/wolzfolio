14740f4f134598d1635ac65122bd1a713c1ed8ec
Wolz <wceliz.dev@gmail.com>
2023-04-27 11:40:48 -0400

    feat: sides and banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import LeftSide from "@/components/LeftSide"
 import Navbar from "@/components/Navbar"
+import RightSide from "@/components/RightSide"
 import Head from "next/head"
+import { motion } from "framer-motion"
+import Banner from "@/components/Banner"
 
 export default function Home() {
   return (
@@ -14,11 +17,31 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <div>
+          <motion.div
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          >
             <LeftSide />
+          </motion.div>
+          <div className="h-[88vh] w-full mx-auto p-4 ">
+            <Banner />
+            {/* About me*/}
+            {/* Experience */}
+            {/* Projects */}
+            {/* Archive */}
+            {/* Contact */}
+            {/* Footer */}
           </div>
-          <div></div>
-          <div></div>
+          <motion.div
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          >
+            <RightSide />
+          </motion.div>
         </div>
       </main>
     </>
